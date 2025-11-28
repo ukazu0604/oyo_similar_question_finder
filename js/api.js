cdd1ee9ca9f71b009d3036837fee6e0aeca560e1
kazu <u.kazu0604@gmail.com>
2025-11-29 02:09:25 +0900

    refactor: 類似度計算ロジックの調整

--- js/api.js
+++ js/api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de0123225ff5b97cce3a135bf8fcc9ca8f9c49e932179405baa54d6fb155eda6
+oid sha256:253d5f0c8fabcdef4b4f6df726e99408bb573d15c8b7e6eeba1859dedf790e29
 size 1798
