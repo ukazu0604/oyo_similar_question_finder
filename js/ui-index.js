577a43ed8d37d144cff0628c420a0181eefc5245
kazu <u.kazu0604@gmail.com>
2025-11-29 12:14:58 +0900

    feat: 中・大カテゴリの進捗計算をアーカイブに対応
    - `js/ui-index.js`において、中項目および大項目の進捗計算ロジックを修正。
    - アーカイブされた問題を適切に進捗に含めるように変更し、全体進捗バーと同様の振る舞いを実現。

--- js/ui-index.js
+++ js/ui-index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50cad6688e1d6da6eb7fffc833b86f6d66bf765c8f8bce0cdd4613fe0c459517
-size 10318
+oid sha256:885c91289f8a90020fa9476b0b5f8a82bf1a4adbb23fab9e8a901dd19a730bfe
+size 11989
