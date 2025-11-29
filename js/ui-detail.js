ffd58fb23822d5b79a1c5a8f83ea900c89f0ec36
kazu <u.kazu0604@gmail.com>
2025-11-29 10:17:18 +0900

    feat(ui): Remove limit on displaying similar problems
    Previously, the display of similar problems was limited to the top 5,
    but this has been removed to display all extracted similar problems.

--- js/ui-detail.js
+++ js/ui-detail.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a6a8a7ba5a162449da954add725326d4050653d958aa36c7a988fe83aa5b2d3
-size 18317
+oid sha256:77698dad3616e5f6920be37e49fa618a8da29cb217941a9d91ffa4e4a3833ab7
+size 18274
