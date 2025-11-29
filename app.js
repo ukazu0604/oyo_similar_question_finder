f2ba8fd68c2fd267dde15919273cb9f8a29823db
kazu <u.kazu0604@gmail.com>
2025-11-29 19:55:12 +0900

    fix: インデックスページの中項目更新バグを修正
    - `app.js`: `initializeUserSession`関数内のUI再描画ロジックを修正。詳細ビューの表示判定条件が不正確だったため、初期ロード時にインデックスページの中項目が正しく更新されない問題を解消しました。
    
    この修正により、ページ読み込み後にユーザーデータが適用された中項目が適切に表示されるようになります。

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5b18ac37c4b2eba106e104affdd4afde2ddc1e8f15fef1d053f0a91847f4010
-size 12045
+oid sha256:7da7875d85f2be62a00e5a494cc2a36a07abfada114b5d87f684e36d9d407f7c
+size 12583
